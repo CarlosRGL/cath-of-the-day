fbeeddbf9521a50f73610b5cf04a3a66bfbf6e67
CarlosRGL <carlosruizgl@gmail.com>
2018-10-15 15:37:11 +0200

    👌 IMPROVE: finsih storepicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  gotToStore = event => {
+    // 1. Stop form from submitting
+    event.preventDefault();
+    // 2. get text from input
+    const storeName = this.myInput.current.value;
+    // 3. Change the page
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={this.gotToStore}>
         <h2>Please enter a store</h2>
         <input
           type="text"
+          ref={this.myInput}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
