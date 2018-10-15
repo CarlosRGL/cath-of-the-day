225371c27605c77d5c771d0c2b1fa90790b6699c
CarlosRGL <carlosruizgl@gmail.com>
2018-10-15 14:42:41 +0200

    📦 NEW: create some components

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="top">
+        <h1>
+          cath
+          <span className="ofThe">
+            <span class="of">of</span> <span class="the">the</span>
+          </span>
+          day
+        </h1>
+        <h3 className="tagline">
+          <span>Fresh Daily</span>
+        </h3>
+      </header>
+    );
+  }
+}
+
+export default Header;
