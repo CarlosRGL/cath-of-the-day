6b1ff2c5fa4ce0d8c4b52ddd8cf902de8893da99
CarlosRGL <carlosruizgl@gmail.com>
2018-10-15 17:22:45 +0200

    📦 NEW: add sample fishes

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   state = {
@@ -16,6 +17,10 @@ class App extends React.Component {
     // 3. set the new fishes object to state
     this.setState({ fishes });
   };
+
+  loadSamplesFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -23,7 +28,10 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSamplesFishes={this.loadSamplesFishes}
+        />
       </div>
     );
   }
